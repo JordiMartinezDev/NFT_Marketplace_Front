b8b9f611b6823527677d8b79d9959d2e60088d9e
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-19 08:18:59 +0100

    Dashboard - Create collection form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import { getXataClient } from "@/xata";
 import React from "react";
+import CreateForm from "./createForm";
 
 type Props = {};
+
+const handleSubmit = (formData: FormData) => {
+  // Handle form submission here
+  console.log(formData);
+};
 
 export default async function DashboardPage({}: Props) {
   const xataClient = getXataClient();
@@ -11,6 +17,7 @@ export default async function DashboardPage({}: Props) {
       {folders.map((folder) => (
         <p key={folder.id}>{folder.NFTname}</p>
       ))}
+      <CreateForm onSubmit={handleSubmit}></CreateForm>
     </div>
   );
 }
