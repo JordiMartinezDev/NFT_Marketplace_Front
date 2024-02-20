1194c25bfeaaa03a4cb1f6aca2b009f2becf53d5
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-20 17:33:09 +0100

    Dashboard - File upload completed and working

--- src/app/dashboard/uploadToDB.ts
+++ src/app/dashboard/uploadToDB.ts
@@ -1,58 +1,3 @@
-// "use server";
-
-// import { getXataClient } from "@/xata";
-// import { auth } from "@clerk/nextjs";
-// import { z } from "zod";
-
-// const MAX_FILE_SIZE = 5000000;
-// const ACCEPTED_IMAGE_TYPES = [
-//   "image/jpeg",
-//   "image/jpg",
-//   "image/png",
-//   "image/webp",
-// ];
-
-// const schema = z.object({
-//   total_NFTs: z.number().min(1),
-//   name: z.string().min(5),
-//   creator_fee: z.number(),
-//   userId: z.string(),
-//   image: z
-//     .any()
-//     .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
-//     .refine(
-//       (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-//       "Only .jpg, .jpeg, .png and .webp formats are supported."
-//     ),
-// });
-
-// async function uploadToDB(formData: FormData) {
-//   const name = formData.get("collectionName");
-//   const numNFTs = Number(formData.get("numNFTs")); // Convert string to number
-//   const creatorFee = Number(formData.get("creatorFee")); // Convert string to number
-//   const image = formData.get("image");
-//   const xataClient = getXataClient();
-
-//   const { userId } = auth();
-
-//   const fileObject = { content: image };
-//   const parsedForm = schema.parse({
-//     total_NFTs: numNFTs,
-//     name: name,
-//     creator_fee: creatorFee,
-//     image: image,
-//     userId: userId,
-//   });
-
-//   await xataClient.db.CreatedCollections.create(parsedForm);
-// }
-
-// export default uploadToDB;
-
-// Above code working without correct file upload
-
-// Following code with security concerns but seems not to have any errors
-
 "use server";
 import { getXataClient } from "@/xata";
 import { z } from "zod";
@@ -87,7 +32,7 @@ async function uploadToDB(formData: FormData) {
   const fileObject = {
     name: name,
     mediaType: image.type,
-    base64Content: "asfasf23r324fergfe43",
+    base64Content: "",
   };
 
   const parsedForm = schema.parse({
