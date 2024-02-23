2f1443ed7cc18f09ed3641d24453e390fb16de07
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-23 08:11:08 +0100

    create page cards

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -4,11 +4,27 @@ import Link from "next/link";
 
 type Props = {};
 
-function page({}: Props) {
-  return <div></div>;
+function Page({}: Props) {
+  const cardStyle = "bg-white border-lg rounded-lg w-1/2";
+
+  return (
+    <div>
+      <Link href={"/createCollection"}>
+        <p className="bg-gray-100 rounded-lg shadow-md transition-transform transform hover:-translate-y-1 hover:scale-105 hover:bg-gray-200 cursor-pointer hover:contrast-125 hover:shadow2xl mb-2">
+          Create Collection
+        </p>
+      </Link>
+
+      <Link href={"/createNFTs"}>
+        <p className="bg-gray-100 rounded-lg shadow-md transition-transform transform hover:-translate-y-1 hover:scale-105 hover:bg-gray-200 cursor-pointer hover:contrast-125 hover:shadow2xl">
+          Create NFT
+        </p>
+      </Link>
+    </div>
+  );
 }
 
-export default page;
+export default Page;
 // <div>
 //   <div>
 //     <Link href={"/createCollection"}> Create Collection</Link>
